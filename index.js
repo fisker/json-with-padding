f462942c9683386b8fb6e829b231d61bcad43523
fisker Cheung <lionkay@gmail.com>
2022-03-17 16:30:35 +0800

    Minor refactor

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ import loadScript from 'browser-load-script'
 const randomString = () =>
   (Math.random() * Date.now()).toString(16).replace('.', '').slice(0, 7)
 
+const CALLBACK_FUNCTION_NAME_PREFIX = 'jsonp_callback_'
+const DEFAULT_CALLBACK_PARAMETER_NAME = 'callback'
+
 async function requestJsonp(url, options) {
   const {callback: callbackParameterName} = {
-    callback: 'callback',
+    callback: DEFAULT_CALLBACK_PARAMETER_NAME,
     ...options,
   }
 
-  url += url.includes('?') ? '&' : '?'
-  url += callbackParameterName
-  url += '='
+  const callbackFunctionName = CALLBACK_FUNCTION_NAME_PREFIX + randomString()
+  const separator = url.includes('?') ? '&' : '?'
+  url += `${separator}${callbackParameterName}=${callbackFunctionName}`
 
-  const callbackFunctionName = `jsonp_callback_${randomString()}`
   let data
   window[callbackFunctionName] = (callbackData) => {
     data = callbackData
   }
 
   try {
-    await loadScript(url + callbackFunctionName)
+    await loadScript(url)
   } finally {
     delete window[callbackFunctionName]
   }
